06b32faca715cdccc716df792afd7f2108abd40d
Leandro <leandrosolano1710@outlook.com>
2025-12-05 17:26:52 -0500

    Inicializar proyecto Spring Boot con dependencias base

---
+++ src/main/java/com/solano/ecommerce/bodegabackend/BodegaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.solano.ecommerce.bodegabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BodegaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BodegaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solano/ecommerce/bodegabackend/BodegaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solano.ecommerce.bodegabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BodegaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
